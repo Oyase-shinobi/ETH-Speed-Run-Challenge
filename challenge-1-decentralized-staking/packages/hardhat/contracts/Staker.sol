92bbff690bec4fc05d64f8d04b745c547bb37f47
Oyase-shinobi <donjosy305@gmail.com>
2024-09-26 22:40:58 +0100

    contract

--- challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
+++ challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,88 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+	ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+  bool executed;
+	bool openWithdraw;
+  uint256 public deadline = block.timestamp + 1 days;
+	uint256 public constant THRESHOLD = 1 ether;
 
-  constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+  mapping(address => uint) public balances;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+	error StakeMustNotBeZero();
+	error AddrZeroDetected();
+	error CanNotExecute();
+	error ThresholdMet();
+	error ThresholdNotMet();
+	error NoStakingYet();
+	error StakingDeadlineHasPasssed();
+	error ExecutedAlready();
+	error StakingOngoing();
 
+	event Stake(address, uint256);
+	event StakeWithdrawn(address);
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+	constructor(address exampleExternalContractAddress) {
+		exampleExternalContract = ExampleExternalContract(
+			exampleExternalContractAddress
+		);
+	}
 
+	// Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+	// (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+	function stake() public payable {
+		if (msg.sender == address(0)) revert AddrZeroDetected();
+		if (msg.value <= 0) revert StakeMustNotBeZero();
+		if (executed) revert StakingDeadlineHasPasssed();
 
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+		balances[msg.sender] += msg.value;
 
+		emit Stake(msg.sender, msg.value);
+	}
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+	// After some `deadline` allow anyone to call an `execute()` function
+	// If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+	function execute() external {
+		if (deadline > block.timestamp) revert CanNotExecute();
+		if (executed) revert ExecutedAlready();
 
+		executed = true;
 
-  // Add the `receive()` special function that receives eth and calls stake()
+		if (address(this).balance >= THRESHOLD) {
+			exampleExternalContract.complete{ value: address(this).balance }();
+		}
+	}
 
+	// If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+	function withdraw() external {
+		if (msg.sender == address(0)) revert AddrZeroDetected();
+		if (address(this).balance >= THRESHOLD) revert ThresholdMet();
+		if (block.timestamp < deadline) revert StakingOngoing();
+		if (balances[msg.sender] <= 0) revert NoStakingYet();
+
+    uint _amtToWithdraw = balances[msg.sender];
+		balances[msg.sender] = 0;
+
+		payable(msg.sender).transfer(_amtToWithdraw);
+
+		emit StakeWithdrawn(msg.sender);
+	}
+
+	// Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+	function timeLeft() external view returns (uint256) {
+		if (block.timestamp >= deadline) return 0;
+
+		return (deadline - block.timestamp);
+	}
+
+	// Add the `receive()` special function that receives eth and calls stake()
+	receive() external payable {
+		if (executed) revert ExecutedAlready();
+		stake();
+	}
 }
