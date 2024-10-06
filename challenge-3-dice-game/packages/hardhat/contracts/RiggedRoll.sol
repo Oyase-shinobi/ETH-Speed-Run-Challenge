43a262c91a629e3dea41fd57a88358802dd88291
Oyase-shinobi <donjosy305@gmail.com>
2024-10-06 12:40:12 +0100

    contract

--- challenge-3-dice-game/packages/hardhat/contracts/RiggedRoll.sol
+++ challenge-3-dice-game/packages/hardhat/contracts/RiggedRoll.sol
@@ -9,15 +9,36 @@ contract RiggedRoll is Ownable {
 
     DiceGame public diceGame;
 
+    event Roll(address indexed player, uint256 amount, uint256 roll);
+
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
-
     // Implement the `withdraw` function to transfer Ether from the rigged contract to a specified address.
+    function withdraw(address _addr, uint256 _amount) external onlyOwner {
+        require(_addr != address(0), "Invalid address");
+        require(address(this).balance >= _amount, "Insufficient balance in the contract");
+        
+        (bool success, ) = _addr.call{value: _amount}("");
+        require(success, "Transfer failed");
+    }
 
     // Create the `riggedRoll()` function to predict the randomness in the DiceGame contract and only initiate a roll when it guarantees a win.
+    function riggedRoll() external onlyOwner {
+        require(address(this).balance >= 0.002 ether, "Insufficient balance in the contract");
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce));
+        uint256 roll = uint256(hash) % 16;
+
+        if (roll <= 5) {
+            diceGame.rollTheDice{value: 0.002 ether}();
+            emit Roll(msg.sender, 0.002 ether, roll);
+        } else {
+            revert("Dice roll greater than 5");
+        }
+    }
 
     // Include the `receive()` function to enable the contract to receive incoming Ether.
-
+    receive() external payable {}
 }
--- challenge-3-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.ts
+++ challenge-3-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.ts
@@ -20,11 +20,14 @@ const deployRiggedRoll: DeployFunction = async function (hre: HardhatRuntimeEnvi
   const riggedRoll: RiggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
   // Please replace the text "Your Address" with your own address.
-  // try {
-  //   await riggedRoll.transferOwnership("Your Address");
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  const currentAccount = await ethers.getSigners();
+  const currentAccountAddress = currentAccount[0].address;
+  try {
+    console.log(currentAccountAddress, deployer);
+    await riggedRoll.transferOwnership("0x40feacdeee6f017fA2Bc1a8FB38b393Cf9022d71");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default deployRiggedRoll;
