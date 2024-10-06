f117cf1988292404b3bafa636fc0217eceac12ea
Oyase-shinobi <donjosy305@gmail.com>
2024-10-06 17:41:29 +0100

    contracts

--- challenge-4-dex/packages/hardhat/deploy/00_deploy_your_contract.ts
+++ challenge-4-dex/packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -2,6 +2,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { DEX } from "../typechain-types/contracts/DEX";
 import { Balloons } from "../typechain-types/contracts/Balloons";
+import { ethers } from "hardhat";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -49,20 +50,21 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
 
   const dex = (await hre.ethers.getContract("DEX", deployer)) as DEX;
 
-  // // paste in your front-end address here to get 10 balloons on deploy:
-  // await balloons.transfer("YOUR_FRONTEND_ADDRESS", "" + 10 * 10 ** 18);
-
+  const currentAccount = await ethers.getSigners();
+  const currentAccountAddress = currentAccount[0].address;
+  console.log(deployer, currentAccountAddress);
+  await balloons.transfer(currentAccountAddress, "" + 10 * 10 ** 18);
   // // uncomment to init DEX on deploy:
 
-  // const dexAddress = await dex.getAddress();
-  // console.log("Approving DEX (" + dexAddress + ") to take Balloons from main account...");
-  // // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dexAddress, hre.ethers.parseEther("100"));
-  // console.log("INIT exchange...");
-  // await dex.init(hre.ethers.parseEther("5"), {
-  //   value: hre.ethers.parseEther("5"),
-  //   gasLimit: 200000,
-  // });
+  const dexAddress = await dex.getAddress();
+  console.log("Approving DEX (" + dexAddress + ") to take Balloons from main account...");
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dexAddress, hre.ethers.parseEther("100"));
+  console.log("INIT exchange...");
+  await dex.init(hre.ethers.parseEther("5"), {
+    value: hre.ethers.parseEther("5"),
+    gasLimit: 200000,
+  });
 };
 
 export default deployYourContract;
